10547da61bd2f581b7fb989f08163a9664f189b8
Antonio Carlos Souza Ferreira Junior <60859505359@serpro1556542v1.SERPRO-RLSL>
2020-02-20 16:50:41 -0300

    add comments

--- queries.js
+++ queries.js
@@ -1,3 +1,4 @@
+//connection with database PostgreSQL
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user:"postgres",
@@ -6,7 +7,9 @@ const pool = new Pool({
     password:"mata7galinha@",
     port: 5432
 });
+//-----------------------------------
 
+//query to list all users from table USER
 const getUsers = (req, res) => {
     pool.query('SELECT * FROM public."USER" ORDER BY id_user ASC', (error, results) => {
         if(error){
@@ -16,6 +19,7 @@ const getUsers = (req, res) => {
     });
 };
 
+//query to show one user by id
 const getUserById = (req, res) => {
     const id_user = parseInt(req.params.id);
 
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
+//call the arq. that contain the db's queries
 const db = require("./queries");
 
-
+//routes for each query
 app.get("/users", db.getUsers);
 app.get("/users/:id", db.getUserById);
 
